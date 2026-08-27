3d4f6482dc55496fdd78b958003c53e2aa7902b7
UvanBattery01 <ocanasebastian56@gmail.com>
2026-08-27 16:11:09 -0600

    Crear supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// UVAN METAS - Configuración Supabase
+
+const SUPABASE_URL = "https://rbucdijidzxvkxbrimwh.supabase.co";
+
+const SUPABASE_ANON_KEY =
+  "sb_publishable_SfsU0-sUu19pjQRoeWD2Yw_O0UQdn_M";
