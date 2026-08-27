b3bc593b137c4472e66491b89756cc7f39775060
UvanBattery01 <ocanasebastian56@gmail.com>
2026-08-27 15:50:25 -0600

    Actualizar supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,3 @@
-const SUPABASE_URL = "https://rbucdijidzxvkxbrimwh.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_SfsU0-sUu19pjQRoeWD2Yw_O0UQdn_M";
+window.SUPABASE_URL = "https://rbucdijiidzxvkxbrimwh.supabase.co";
+
+window.SUPABASE_ANON_KEY = "sb_publishable_SfsU0-sUu19pjQRoeWD2Yw_0UQdn_M";
