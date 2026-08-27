4bfda29e9ab4418105fe4a1c768adb8171da5b27
UvanBattery01 <ocanasebastian56@gmail.com>
2026-08-26 23:02:08 -0600

    Crear data.js

---
+++ data.js
@@ -0,0 +1,93 @@
+const UVAN_DATA = {
+  mes: "Agosto 2026",
+
+  sucursales: [
+    {
+      nombre: "Chicoloapan",
+      meta: 185,
+      vendedores: [
+        {
+          nombre: "Chuchin",
+          ventas: 0
+        }
+      ]
+    },
+
+    {
+      nombre: "Ixtapaluca",
+      meta: 70,
+      vendedores: [
+        {
+          nombre: "Alejandro",
+          ventas: 21
+        }
+      ]
+    },
+
+    {
+      nombre: "Texcoco",
+      meta: 60,
+      vendedores: [
+        {
+          nombre: "Christopher",
+          ventas: 21
+        }
+      ]
+    },
+
+    {
+      nombre: "Chimalhuacán",
+      meta: 40,
+      vendedores: [
+        {
+          nombre: "Owen",
+          ventas: 13
+        }
+      ]
+    },
+
+    {
+      nombre: "Santa Rosa",
+      meta: 40,
+      vendedores: [
+        {
+          nombre: "Kateryn",
+          ventas: 14
+        }
+      ]
+    },
+
+    {
+      nombre: "Central de Abastos",
+      meta: 56,
+      vendedores: [
+        {
+          nombre: "Araceli",
+          ventas: 8
+        }
+      ]
+    },
+
+    {
+      nombre: "Nezahualcóyotl",
+      meta: 60,
+      vendedores: [
+        {
+          nombre: "Javier",
+          ventas: 18
+        }
+      ]
+    },
+
+    {
+      nombre: "Los Reyes",
+      meta: 56,
+      vendedores: [
+        {
+          nombre: "Angel",
+          ventas: 14
+        }
+      ]
+    }
+  ]
+};
